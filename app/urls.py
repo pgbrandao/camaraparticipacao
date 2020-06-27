68a54a934252d6f9a6a3cf2f1948f6c31890498b
Pedro G Brandao <pgbrandao@MacBook-Pro.local>
2020-06-27 14:01:15 -0300

    Initial version of new poll search: by date

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('enquetes/busca_data/', views.enquetes_busca_data, name='enquetes_busca_data'),
     path('proposicao/<int:id_proposicao>/', views.proposicao_detail, name='proposicao_detail')
 ]
--- app/views.py
+++ app/views.py
@@ -7,9 +7,6 @@ from . import plots
 
 from .models import *
 
-def is_valid_queryparam(param):
-    return param != '' and param is not None
-
 def BootstrapFilterView(request):
     sigla_tipo = request.GET.get('sigla_tipo')
     numero = request.GET.get('numero')
@@ -59,8 +56,8 @@ def index(request):
     posicionamentos_past_week = Posicionamento.objects.filter(dat_posicionamento__gte=dm14, dat_posicionamento__lt=dm7).count()
     posicionamentos_change = (posicionamentos_this_week / posicionamentos_past_week - 1) * 100 if posicionamentos_past_week else 0
 
-    proposicao_pageview_this_week = ProposicaoPageview.objects.filter(date__gte=dm7, date__lt=d0).aggregate(Sum('pageviews'))['pageviews__sum']
-    proposicao_pageview_past_week = ProposicaoPageview.objects.filter(date__gte=dm14, date__lt=dm7).aggregate(Sum('pageviews'))['pageviews__sum']
+    proposicao_pageview_this_week = ProposicaoPageview.objects.filter(date__gte=dm7, date__lt=d0).aggregate(Sum('pageviews'))['pageviews__sum'] or 0
+    proposicao_pageview_past_week = ProposicaoPageview.objects.filter(date__gte=dm14, date__lt=dm7).aggregate(Sum('pageviews'))['pageviews__sum'] or 0  
     proposicao_pageview_change = (proposicao_pageview_this_week / proposicao_pageview_past_week - 1) * 100 if proposicao_pageview_past_week else 0
 
     daily_summary_global_plot = plots.daily_summary_global()
@@ -77,6 +74,34 @@ def index(request):
         proposicoes = proposicoes[:50]
 
     return render(request, 'index.html', locals())
+
+def enquetes_busca_data(request):
+    date_min = None
+    try:
+        date_min = datetime.datetime.strptime(request.GET.get('date_min'), '%d/%m/%Y')
+    except (ValueError, TypeError) as e:
+        date_min = datetime.date.today()-datetime.timedelta(days=14)
+    
+    date_max = None
+    try:
+        date_max = datetime.datetime.strptime(request.GET.get('date_max'), '%d/%m/%Y')
+    except (ValueError, TypeError) as e:
+        date_max = datetime.date.today()-datetime.timedelta(days=1)
+
+    qs = ProposicaoAggregated.objects.filter(date__gte=date_min, date__lte=date_max) \
+        .values('proposicao') \
+        .annotate(poll_votes_total=Sum('poll_votes'), poll_comments_total=Sum('poll_comments')) \
+        .values('proposicao__id', 'proposicao__sigla_tipo', 'proposicao__numero', 'proposicao__ano', 'poll_votes_total', 'poll_comments_total')
+    
+    proposicoes = pd.DataFrame.from_records(qs)
+    if not proposicoes.empty:
+        proposicoes['p_score'] = proposicoes['poll_votes_total'] + proposicoes['poll_comments_total']
+        proposicoes.sort_values(by=['p_score'], ascending=False, inplace=True)
+        proposicoes = proposicoes[:50]
+
+    return render(request, 'enquetes_busca_data.html', locals())
+
+    
 
 def proposicao_detail(request, id_proposicao):
     proposicao = Proposicao.objects.get(pk=id_proposicao)
