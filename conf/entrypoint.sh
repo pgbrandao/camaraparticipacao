6c1797b7173bdeabb18ee8220e52994f474b1f4b
Pedro Brandão <pgbrandao@gmail.com>
2020-07-13 17:31:19 -0300

    Fixed typo; added pending migration

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -3,6 +3,6 @@
 # python manage.py flush --no-input
 python manage.py migrate
 mkdir -p static
-python manage.py collectstatic --no-input --clear -verbosity 0
+python manage.py collectstatic --no-input --clear --verbosity 0
 
 exec "$@"
