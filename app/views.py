4b7b544f5192702a1a9b983fc618a1a1d83f7df0
Pedro Brandão <pgbrandao@gmail.com>
2021-01-11 15:05:19 -0300

    Simple report caching (needs improving)

--- app/views.py
+++ app/views.py
@@ -220,101 +220,108 @@ def relatorio_consolidado(request, custom):
 
 
     if initial_date and final_date:
-        stats = {}
+        cache_name = 'relatorio_consolidado-{}-{}'.format(initial_date.toordinal(), final_date.toordinal())
+        stats = cache.get(cache_name, None)
 
-        # summary plots
-        stats['ficha_enquete_summary_plot'] = plots.summary_plot(group_by='day', height=340, initial_date=initial_date, final_date=final_date, subplots=['ficha', 'enquete'], show_legend=True)
-        stats['prisma_summary_plot'] = plots.summary_plot(group_by='day', height=200, initial_date=initial_date, final_date=final_date, subplots=['prisma'], show_legend=True)
-        stats['noticia_summary_plot'] = plots.summary_plot(group_by='day', height=200, initial_date=initial_date, final_date=final_date, subplots=['noticia'], show_legend=True)
+        if not stats:
 
-        # enquetes votes and comments
-        qs = ProposicaoAggregated.objects.get_aggregated(initial_date, final_date)
+            stats = {}
 
-        stats.update({
-            'poll_votes': qs['poll_votes'],
-            'poll_comments': qs['poll_comments'],
-            'poll_comments_unchecked': qs['poll_comments_unchecked'],
-            'poll_comments_authorized': qs['poll_comments_authorized'],
-            'poll_comments_unauthorized': qs['poll_comments_unauthorized'],
-        })
+            # summary plots
+            stats['ficha_enquete_summary_plot'] = plots.summary_plot(group_by='day', height=340, initial_date=initial_date, final_date=final_date, subplots=['ficha', 'enquete'], show_legend=True)
+            stats['prisma_summary_plot'] = plots.summary_plot(group_by='day', height=200, initial_date=initial_date, final_date=final_date, subplots=['prisma'], show_legend=True)
+            stats['noticia_summary_plot'] = plots.summary_plot(group_by='day', height=200, initial_date=initial_date, final_date=final_date, subplots=['noticia'], show_legend=True)
 
-        # portal comments
-        comentarios_camara_count = PortalComentario.objects.get_comentarios_camara_count(initial_date, final_date)
-        qs = NoticiaAggregated.objects.get_aggregated(initial_date, final_date)
+            # enquetes votes and comments
+            qs = ProposicaoAggregated.objects.get_aggregated(initial_date, final_date)
 
-        stats.update({
-            'portal_comments': qs['portal_comments'],
-            'portal_comments_unchecked': qs['portal_comments_unchecked'],
-            'portal_comments_authorized': qs['portal_comments_authorized'],
-            'portal_comments_unauthorized': qs['portal_comments_unauthorized'],
-            'portal_comments_camara': comentarios_camara_count,
-        })
+            stats.update({
+                'poll_votes': qs['poll_votes'],
+                'poll_comments': qs['poll_comments'],
+                'poll_comments_unchecked': qs['poll_comments_unchecked'],
+                'poll_comments_authorized': qs['poll_comments_authorized'],
+                'poll_comments_unauthorized': qs['poll_comments_unauthorized'],
+            })
 
-        # prisma tickets
-        qs = PrismaDemanda.objects.get_count(initial_date, final_date)
+            # portal comments
+            comentarios_camara_count = PortalComentario.objects.get_comentarios_camara_count(initial_date, final_date)
+            qs = NoticiaAggregated.objects.get_aggregated(initial_date, final_date)
 
-        stats.update({
-            'prisma_tickets': qs['iddemanda__count'],
-        })
+            stats.update({
+                'portal_comments': qs['portal_comments'],
+                'portal_comments_unchecked': qs['portal_comments_unchecked'],
+                'portal_comments_authorized': qs['portal_comments_authorized'],
+                'portal_comments_unauthorized': qs['portal_comments_unauthorized'],
+                'portal_comments_camara': comentarios_camara_count,
+            })
 
-        # prisma formas de recebimento
-        qs = PrismaDemanda.objects.get_forma_de_recebimento_counts(initial_date, final_date)
+            # prisma tickets
+            qs = PrismaDemanda.objects.get_count(initial_date, final_date)
 
-        stats.update({
-            'prisma_formas_de_recebimento': [row for row in qs]
-        })
+            stats.update({
+                'prisma_tickets': qs['iddemanda__count'],
+            })
 
-        # prisma tipos
-        qs = PrismaDemanda.objects.get_tipo_counts(initial_date, final_date)
+            # prisma formas de recebimento
+            qs = PrismaDemanda.objects.get_forma_de_recebimento_counts(initial_date, final_date)
 
-        stats.update({
-            'prisma_tipos': [row for row in qs]
-        })
+            stats.update({
+                'prisma_formas_de_recebimento': [row for row in qs]
+            })
+
+            # prisma tipos
+            qs = PrismaDemanda.objects.get_tipo_counts(initial_date, final_date)
+
+            stats.update({
+                'prisma_tipos': [row for row in qs]
+            })
 
 
-        # prisma categorias
-        prisma_categorias = PrismaDemanda.objects.get_categoria_counts(initial_date, final_date)
+            # prisma categorias
+            prisma_categorias = PrismaDemanda.objects.get_categoria_counts(initial_date, final_date)
 
-        stats.update({
-            'top_prisma_categorias': [row for row in prisma_categorias]
-        })
+            stats.update({
+                'top_prisma_categorias': [row for row in prisma_categorias]
+            })
 
-        # prisma proposições
-        qs = PrismaDemanda.objects.get_proposicao_counts(initial_date, final_date)
+            # prisma proposições
+            qs = PrismaDemanda.objects.get_proposicao_counts(initial_date, final_date)
 
-        stats.update({
-            'top_prisma_proposicoes': [row for row in qs]
-        })
+            stats.update({
+                'top_prisma_proposicoes': [row for row in qs]
+            })
 
-        # prisma sexo
-        prisma_sexo_idade_plot = plots.prisma_sexo_idade(initial_date, final_date)
-        stats.update({
+            # prisma sexo
+            prisma_sexo_idade_plot = plots.prisma_sexo_idade(initial_date, final_date)
+            stats.update({
                 'prisma_sexo_idade_plot': prisma_sexo_idade_plot,
-        })
+            })
 
 
-        # top polls
-        qs = ProposicaoAggregated.objects.top_proposicoes(initial_date, final_date)[:500]
-        stats.update({
-            'top_proposicoes': [{
-                'proposicao_nome_processado': row['proposicao__nome_processado'],
-                'link': reverse('proposicao_detail', args=[row['proposicao__id']]),
-                'poll_votes': row['poll_votes'],
-                'poll_comments': row['poll_comments'],
-                'ficha_pageviews': row['ficha_pageviews'],
-            } for row in qs]
-        })
+            # top polls
+            qs = ProposicaoAggregated.objects.top_proposicoes(initial_date, final_date)[:500]
+            stats.update({
+                'top_proposicoes': [{
+                    'proposicao_nome_processado': row['proposicao__nome_processado'],
+                    'link': reverse('proposicao_detail', args=[row['proposicao__id']]),
+                    'poll_votes': row['poll_votes'],
+                    'poll_comments': row['poll_comments'],
+                    'ficha_pageviews': row['ficha_pageviews'],
+                } for row in qs]
+            })
 
-        # top news
-        qs = PortalComentario.objects.get_top_news(initial_date, final_date)[:500]
+            # top news
+            qs = PortalComentario.objects.get_top_news(initial_date, final_date)[:500]
 
-        stats.update({
-            'top_news': [{
-                'title': row['url__titulo'] or '',
-                'link': row['url__link'] or '',
-                'comments': row['comments'],
-            } for row in qs]
-        })
+            stats.update({
+                'top_news': [{
+                    'title': row['url__titulo'] or '',
+                    'link': row['url__link'] or '',
+                    'comments': row['comments'],
+                } for row in qs]
+            })
+
+            cache.set(cache_name, stats, 86400)
 
 
     if not custom:
