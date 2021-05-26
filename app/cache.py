2a69a9784883548ebf16d795a64cbc3d4e1724f2
Pedro Brandao <pgbrandao@gmail.com>
2021-05-26 13:58:00 -0300

    Changing cache sorting for easier testing.

--- app/cache.py
+++ app/cache.py
@@ -8,7 +8,7 @@ from django.core.cache import cache
 def rebuild_caches():
     from . import reports
 
-    for year_start in pd.date_range(start='2019-01-01', end=datetime.date.today(), freq='YS'):
+    for year_start in pd.date_range(start='2019-01-01', end=datetime.date.today(), freq='YS').sort_values(ascending=False):
         year_end = year_start.replace(month=12, day=31)
         year_start = year_start.to_pydatetime().date()
         year_end = year_end.to_pydatetime().date()
@@ -22,7 +22,7 @@ def rebuild_caches():
         reports.api_relatorio_consolidado(initial_date=year_start, final_date=year_end, save_cache=True)
         reports.relatorio_consolidado(initial_date=year_start, final_date=year_end, save_cache=True)
 
-    for month_start in pd.date_range(start='2019-01-01', end=datetime.date.today(), freq='MS'):
+    for month_start in pd.date_range(start='2019-01-01', end=datetime.date.today(), freq='MS').sort_values(ascending=False):
         month_end = month_start.replace(day=month_start.days_in_month)
 
         month_start = month_start.to_pydatetime().date()
@@ -32,7 +32,7 @@ def rebuild_caches():
         reports.api_top_proposicoes(initial_date=month_start, final_date=month_end, save_cache=True)
         reports.relatorio_consolidado(initial_date=month_start, final_date=month_end, save_cache=True)
 
-    for day in pd.date_range(start='2019-01-01', end=datetime.date.today()):
+    for day in pd.date_range(start='2019-01-01', end=datetime.date.today()).sort_values(ascending=False):
         day = day.to_pydatetime().date()
 
         reports.api_top_noticias(initial_date=day, final_date=day, save_cache=True)
